ddd798e8b670221114cad1c9f95f2b14fb18ec0a
lucasfer86 <105600500+lucasfer86@users.noreply.github.com>
2023-06-22 11:47:58 -0300

    Update BookingController.java

---
+++ src/main/java/org/example/Controller/BookingController.java
@@ -0,0 +1,148 @@
+package org.example.Controller;
+
+import org.example.entity.Booking;
+import org.example.repository.BookingRepository;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class BookingController {
+    private final BookingRepository bookingRepository = new BookingRepository();
+
+    /**
+     * Displays the booking menu options.
+     */
+    public static void viewBookingMenu(){
+        System.out.println("\n*-*-*-*-*-*-*-**** UTN Motel ****-*-*-*-*-*-*");
+        System.out.println("*-*-*-*-*-*-*-**** Menu Reserva ****-*-*-*-*-*");
+        System.out.println("1. Check in");
+        System.out.println("2. Check out");
+        System.out.println("3. Ver todas las reservas");
+        System.out.println("4. Servicio al cuarto");
+        System.out.println("0. Salir");
+        System.out.print("Opción: ");
+    }
+    /**
+     * Booking menu options.
+     */
+    public static void bookingMenu(Scanner scanner){
+        BookingController bookingController = new BookingController();
+
+        String option = "";
+
+        while(!option.equals("0")) {
+
+            viewBookingMenu();
+            option = scanner.nextLine();
+
+            switch (option) {
+                case "1" -> bookingController.checkIn();
+                case "2" -> bookingController.checkOut();
+                case "3" -> bookingController.listAll();
+                case "4" -> bookingController.serviceRoom();
+                case "0" -> option = "0";
+                default -> System.out.println("Opción incorrecta.");
+            }
+        }
+    }
+
+    public void checkIn(){
+        System.out.println("Realizando check-in...");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el ID de la reserva: ");
+        String bookingId = scanner.nextLine();
+
+        Booking booking = bookingRepository.getById(bookingId);
+        if (booking == null) {
+            System.out.println("No se encontró la reserva con el ID especificado.");
+            return;
+        }
+
+        if (booking.getBookingState() != BookingState.PENDING) {
+            System.out.println("La reserva ya ha sido check-in o check-out.");
+            return;
+        }
+
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.isBefore(booking.getCheckInDate())) {
+            System.out.println("No es posible realizar el check-in antes de la fecha de check-in programada.");
+            return;
+        }
+
+        booking.setBookingState(BookingState.CHECKED_IN);
+        bookingRepository.update(booking);
+
+        System.out.println("Check-in realizado con éxito.");
+        //System.out.println("1. Check in");
+    }
+
+    public void checkOut(){
+        System.out.println("Realizando check-out...");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el ID de la reserva: ");
+        String bookingId = scanner.nextLine();
+
+        Booking booking = bookingRepository.getById(bookingId);
+        if (booking == null) {
+            System.out.println("No se encontró la reserva con el ID especificado.");
+            return;
+        }
+
+        if (booking.getBookingState() != BookingState.CHECKED_IN) {
+            System.out.println("La reserva no ha sido check-in o ya ha sido check-out.");
+            return;
+        }
+
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.isBefore(booking.getCheckOutDate())) {
+            System.out.println("No es posible realizar el check-out antes de la fecha de check-out programada.");
+            return;
+        }
+
+        booking.setBookingState(BookingState.CHECKED_OUT);
+        bookingRepository.update(booking);
+
+        System.out.println("Check-out realizado con éxito.");
+        System.out.println("2. Check out");
+    }
+    public void listAll(){
+        List<Booking> bookings = bookingRepository.list();
+
+        System.out.println("\n*-*-*-*-*-*-*-**** UTN Motel ****-*-*-*-*-*-*");
+        System.out.println("*-*-*-*-*-*-*-**** Lista Booking ****-*-*-*-*-*");
+
+        if( bookings.size() == 0 ){
+            System.out.println("No hay Bookings cargadas en el sistema");
+            return;
+        }
+
+        System.out.println(bookings);
+    }
+    public void serviceRoom(){
+
+        System.out.println("Solicitando servicio al cuarto...");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el ID de la reserva: ");
+        String bookingId = scanner.nextLine();
+
+        Booking booking = bookingRepository.getById(bookingId);
+        if (booking == null) {
+            System.out.println("No se encontró la reserva con el ID especificado.");
+            return;
+        }
+
+        // Lógica para tomar la orden del servicio
+        System.out.print("Ingrese la orden del servicio: ");
+        String order = scanner.nextLine();
+
+        System.out.println("Servicio al cuarto solicitado con éxito.")
+    }
+
+
+}
